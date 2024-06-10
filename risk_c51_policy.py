c30ff5efdaa6bac4bf360e034afba6e7f602e4b3
Panagiotis Liampas <pliam1105@gmail.com>
2024-06-10 15:51:10 +0300

    Added evaluation and plotting

--- risk_c51_policy.py
+++ risk_c51_policy.py
@@ -5,6 +5,8 @@ import pickle
 from typing import Callable
 
 import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+import scipy.stats as stats
 
 import torch, numpy as np
 from torch import nn, vmap
@@ -170,6 +172,7 @@ class RiskC51Policy(C51Policy):
             # distorted_quantile_grads -> [batch_size, action_size, atom_num], distortion risk measure derivatives at the quantiles
             distorted_quantile_grads = torch.vmap(torch.vmap(batched_quantile_grads))(quantiles)
             # now we want the sum (in the atoms' dimension) of element-wise products of logits (quantile differences), distorted_quantile_grads, and used_values
+            assert distorted_quantile_grads.shape == logits.shape
             distorted_expectation = torch.sum(logits*distorted_quantile_grads*used_values, 2)
             assert distorted_expectation.shape == logits.shape[:2]
             return DQNPolicy.compute_q_value(self, distorted_expectation, mask)
@@ -182,17 +185,28 @@ def CVaR(quantiles: torch.Tensor, beta: float) -> torch.Tensor:
     return 1.0/beta*torch.le(quantiles, torch.full_like(quantiles, beta))
 
 def Wang(quantiles: torch.Tensor, beta: float) -> torch.Tensor:
-    x = torch.vmap(torch.distributions.Normal(torch.tensor([0.0]), torch.tensor([1.0])).icdf)(quantiles)
+    x = torch.vmap(torch.distributions.Normal(torch.tensor([0.0]), torch.tensor([1.0])).icdf)(quantiles)[:,0]
     return torch.exp(-beta*x-torch.full_like(x,(beta**2)/2))
 
 def CPW(x: torch.Tensor, b: float) -> torch.Tensor:
     return b*(x**(b-1))*((x**b+(1-x)**b)**(-1/b)) - (x**b)*(b*(x**(b-1))-b*((1-x)**(b-1)))*((x**b+(1-x)**b)**(-1/(b-1)))/b
 
+# for a one-dimensional tensor
+def RiskCompute(returns: torch.Tensor, risk_function:Callable[[torch.Tensor, float], torch.Tensor], risk_param: float):
+    assert returns.dim() == 1
+    logits = torch.full_like(returns, 1/returns.shape[0]) # dirac mixture
+    quantiles = torch.cumsum(logits, 0)
+    distorted_quantile_grads = risk_function(quantiles, risk_param)
+    return torch.sum(logits*distorted_quantile_grads*returns)
+
+
 """ PARAMETERS """
 num_train_envs = 10 # change that for number of environments in parallel (although sequentially) in training
 #change min and max values for return range
 num_test_envs = 10
-num_test_episodes = 10
+num_eval_envs = 50
+# num_test_episodes = 10
+# num_eval_episodes = 50
 min_return = -100
 max_return = 100
 
@@ -206,7 +220,7 @@ eps_step_final = 50000
 
 reward_threshold = None
 
-def run_experiment(risk_measure="neutral", beta=0.0):
+def run_experiment(risk_measure="neutral", beta=0.0, load_policy=False, train_policy=False):
     print("RUNNING EXPERIMENT:",risk_measure, beta)
     env = create_env(None)
     env.reset()
@@ -223,13 +237,13 @@ def run_experiment(risk_measure="neutral", beta=0.0):
     optim = torch.optim.Adam(net.parameters(), lr=1e-3)
 
     if risk_measure == "neutral":
-        func = Neutral
+        risk_func = Neutral
     elif risk_measure == "cvar":
-        func = CVaR
+        risk_func = CVaR
     elif risk_measure == "wang":
-        func = Wang
+        risk_func = Wang
     elif risk_measure == "cpw":
-        func = CPW
+        risk_func = CPW
 
     policy: RiskC51Policy = RiskC51Policy(
         model=net,
@@ -242,65 +256,114 @@ def run_experiment(risk_measure="neutral", beta=0.0):
         v_max=max_return,
         estimation_step=3,
         target_update_freq=320,
-        risk_function=func,
+        risk_function=risk_func,
         risk_param=beta
     )
 
-    train_collector = Collector(policy, train_envs, VectorReplayBuffer(20000, num_train_envs), exploration_noise=True)
-    test_collector = Collector(policy, test_envs, exploration_noise=True)
+    if load_policy:
+        policy.load_state_dict(torch.load(risk_measure+'.pth')) # to load existing policy
 
-    #TensorBoard logging
-    writer = SummaryWriter("log/"+risk_measure)
-    logger = TensorboardLogger(writer)
+    if train_policy:
+        train_collector = Collector(policy, train_envs, VectorReplayBuffer(20000, num_train_envs), exploration_noise=True)
+        test_collector = Collector(policy, test_envs, exploration_noise=True)
 
-    def train_fn(epoch: int, env_step: int) -> None:
-        # eps annnealing, just a demo
-        if env_step <= eps_step_init:
-            policy.set_eps(eps_train)
-        elif env_step <= eps_step_final:
-            eps = eps_train - (env_step - 10000) / 40000 * (0.9 * eps_train)
-            policy.set_eps(eps)
-        else:
-            policy.set_eps(0.1 * eps_train)
+        #TensorBoard logging
+        writer = SummaryWriter("log/"+risk_measure)
+        logger = TensorboardLogger(writer)
 
-    trainer = OffpolicyTrainer(
-        policy=policy,
-        train_collector=train_collector,
-        test_collector=test_collector,
-        max_epoch=50, #default: 10, preferred: 20-50? and then it works pretty well
-        step_per_epoch=1000, #default: 8000, preferred: 1000-5000
-        step_per_collect=10, #default: 8, preferred: 10
-        # episode_per_collect=1,
-        update_per_step=0.125, #default: 0.125
-        episode_per_test=50, #default: 100, preferred: 10-50
-        batch_size=batch_size,
-        train_fn=train_fn,
-        test_fn=lambda epoch, env_step: policy.set_eps(eps_test),
-        logger=logger,
-    )
+        def train_fn(epoch: int, env_step: int) -> None:
+            # eps annnealing
+            if env_step <= eps_step_init:
+                policy.set_eps(eps_train)
+            elif env_step <= eps_step_final:
+                eps = eps_train - (env_step - 10000) / 40000 * (0.9 * eps_train)
+                policy.set_eps(eps)
+            else:
+                policy.set_eps(0.1 * eps_train)
 
-    # policy.load_state_dict(torch.load(risk_measure+'.pth')) # to load existing policy
+        trainer = OffpolicyTrainer(
+            policy=policy,
+            train_collector=train_collector,
+            test_collector=test_collector,
+            max_epoch=50, #default: 10, preferred: 50 (maybe even 100 if slow convergence) and then it works pretty well
+            step_per_epoch=1000, #default: 8000, preferred: 1000
+            step_per_collect=10, #default: 8, preferred: 10
+            # episode_per_collect=1,
+            update_per_step=0.125, #default: 0.125
+            episode_per_test=50, #default: 100, preferred: 50
+            batch_size=batch_size,
+            train_fn=train_fn,
+            test_fn=lambda epoch, env_step: policy.set_eps(eps_test),
+            logger=logger,
+        )
 
-    for epoch_stat in trainer:
-        if(epoch_stat.epoch < trainer.max_epoch -2): # because in the last two epochs it becomes terrible, don't know why
-            torch.save(policy.state_dict(), risk_measure+'.pth') # to save the policy in a file
-            print(epoch_stat)
+        epoch_risk_comps = []
+        for epoch_stat in trainer:
+            if(epoch_stat.epoch < trainer.max_epoch - 2): # because in the last two epochs it becomes terrible, don't know why
+                torch.save(policy.state_dict(), risk_measure+'.pth') # to save the policy in a file
+                print(epoch_stat)
 
-            # TODO: compute my custom evaluation metrics (over the episodes of the current epoch)
-            # from the epoch_stat (and its test returns) and print them, then plot them after the end of training
+                # collect risk measure computation from epoch testing
+                risk_comp = RiskCompute(torch.Tensor(epoch_stat.test_collect_stat.returns), risk_func, beta)
+                # add: (epoch, risk, mean, std, min, max)
+                epoch_risk_comps.append((epoch_stat.epoch, risk_comp, epoch_stat.test_collect_stat.returns_stat.mean, epoch_stat.test_collect_stat.returns_stat.std, epoch_stat.test_collect_stat.returns_stat.min, epoch_stat.test_collect_stat.returns_stat.max))
 
-            # view test episodes (new ones, not those used by the trainer and collected in epoch_stats) in a popup window
-            policy.eval()
-            # policy.set_eps(eps_test)
-            env = create_env("human")
-            buf = VectorReplayBuffer(20000, num_test_envs)
-            collector = ts.data.Collector(policy, env, buf, exploration_noise=True)
-            collector.reset()
-            collector.collect(n_episode=num_test_episodes, render=1 / 200)
-            env.close()
-            # print(np.min(buf.act))
-            # print(np.max(buf.act))
-            policy.train()
+                # view test episodes (new ones, not those used by the trainer and collected in epoch_stats) in a popup window
+                policy.eval()
+                # policy.set_eps(eps_test)
+                env = create_env("human")
+                buf = VectorReplayBuffer(20000, num_test_envs)
+                collector = ts.data.Collector(policy, env, buf, exploration_noise=True)
+                collector.reset()
+                collector.collect(n_episode=num_test_envs, render=1 / 200) # one episode per env
+                env.close()
+                # print(np.min(buf.act))
+                # print(np.max(buf.act))
+                policy.train()
+
+        policy.load_state_dict(torch.load(risk_measure+'.pth')) # to load previous policy before getting worse
+        epochs, risks, means, stds, mins, maxs = zip(*epoch_risk_comps)
+        fig = plt.figure()
+        plt.plot(epochs, risks, label="risk measure")
+        plt.plot(epochs, means, label="mean return")
+        plt.plot(epochs, stds, label="return standard deviation")
+        plt.plot(epochs, mins, label="minimum return")
+        plt.plot(epochs, maxs, label="maximum return")
+        plt.legend()
+        plt.savefig(fname="plots/"+risk_measure+"_training", dpi=fig.dpi)
+        fig.clear()
+    
+    # evaluate trained/stored policy
+    policy.eval()
+    # policy.set_eps(eps_test)
+    env = create_env("human")
+    buf = VectorReplayBuffer(20000, num_eval_envs)
+    collector = ts.data.Collector(policy, env, buf, exploration_noise=True)
+    collector.reset()
+    collect_stats = collector.collect(n_episode=num_eval_envs, render=1 / 200) # one episode per env
+    env.close()
+    risk_comp = RiskCompute(torch.tensor(collect_stats.returns), risk_func, beta)
+    mean = collect_stats.returns_stat.mean
+    std = collect_stats.returns_stat.std
+    min = collect_stats.returns_stat.min
+    max = collect_stats.returns_stat.max
+    print("Risk measure:",risk_comp,"mean:", mean, "std:", std, "min:", min, "max:", max) # print results
+
+    # plot probability distribution
+    fig = plt.figure()
+    # n = collect_stats.returns.size//10
+    n = 10
+    p, x = np.histogram(collect_stats.returns, bins=n, density=True) # bin it into n = N//10 bins
+    x_d = np.diff(x)
+    x = x[:-1] + (x[1] - x[0])/2   # convert bin edges to centers
+    # f = UnivariateSpline(x, p, s=n)
+    # plt.plot(x, f(x))
+    plt.bar(x, p*x_d)
+    x = np.linspace(mean - 3*std, mean + 3*std, 100)
+    plt.plot(x, stats.norm.pdf(x, mean, std)) # normal distribution using the statistics, as an approximation
+    plt.figtext(0.01,0.01,"Risk measure: "+str(round(risk_comp.item(),1))+" mean: "+str(round(mean,1))+" std: "+str(round(std,1))+" min: "+str(round(min,1))+" max: "+str(round(max,1)))
+    plt.savefig(fname="plots/"+risk_measure+"_eval", dpi=fig.dpi)
+    fig.clear()
 
 if __name__ == "__main__":
     # plot distortion risk measure functions to ensure they are correct
@@ -311,7 +374,7 @@ if __name__ == "__main__":
     # plt.plot(input, CPW(input, 0.71))
     # plt.show()
 
-    run_experiment("neutral")
-    run_experiment("cvar", 0.25)
-    run_experiment("wang", 0.75)
-    run_experiment("cpw", 0.71)
+    run_experiment("neutral", load_policy=False, train_policy=True)
+    run_experiment("cvar", 0.25, load_policy=False, train_policy=True)
+    run_experiment("wang", 0.75, load_policy=False, train_policy=True)
+    run_experiment("cpw", 0.71, load_policy=False, train_policy=True)
